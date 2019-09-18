7825ce7b3312b931621ba3fcc4ecd920ff8f32df
Eagle223 <m18875144703@163.com>
2019-09-18 15:19:57 +0800

    "完成了视频流截取，人脸建模，人脸识别"

--- main.go
+++ main.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-	"fmt"
 	"gofaces/router"
 	"gofaces/rtsp"
+	"log"
 	"strconv"
 	"strings"
 	"time"
@@ -18,18 +18,31 @@ func videoCaptureHandler() {
 	var pid string
 	ch := make(chan string)
 	imgDir := strconv.Itoa(time.Now().Hour()) + "-" + strconv.Itoa(time.Now().Day()) + "-" + time.Now().Month().String() + "-" + strconv.Itoa(time.Now().Year())
-	fmt.Println(imgDir)
-	ticker := time.NewTicker(time.Hour * 1)
-	defer ticker.Stop()
+	log.Println(imgDir)
+	go rtsp.VideoCaptureStart1("classify%d.jpg", imgDir, ch)
+	if 0 == strings.Compare("success", <-ch) {
+		ticker1 := time.NewTicker(time.Minute * time.Duration(60-time.Now().Minute()))
+		pid = <-ch
+		log.Println("ticker time out", <-ticker1.C)
+		go rtsp.VideoCaptureStop1(pid, ch)
+		<-ch
+		ticker1.Stop()
+	} else {
+		log.Fatalf("ffmpeg 出错，请检查系统")
+	}
+	ticker := time.NewTicker(time.Hour)
 	for true {
+		imgDir = strconv.Itoa(time.Now().Hour()) + "-" + strconv.Itoa(time.Now().Day()) + "-" + time.Now().Month().String() + "-" + strconv.Itoa(time.Now().Year())
+		log.Println(imgDir)
 		go rtsp.VideoCaptureStart1("classify%d.jpg", imgDir, ch)
 		if 0 == strings.Compare("success", <-ch) {
 			pid = <-ch
-			fmt.Println("ffmpeg pid :", pid)
-			imgDir = (<-ticker.C).String()
-			fmt.Println(imgDir)
-			rtsp.VideoCaptureStop1(pid, ch)
+			log.Println("ffmpeg pid :", pid)
+			log.Println("ticker time out", <-ticker.C)
+			go rtsp.VideoCaptureStop1(pid, ch)
 			<-ch
+		} else {
+			log.Fatalf("ffmpeg 出错，请检查系统")
 		}
 	}
 }
--- rtsp/rtsp.go
+++ rtsp/rtsp.go
@@ -2,8 +2,10 @@ package rtsp
 
 import (
 	"fmt"
+	"github.com/gin-gonic/gin"
 	"io/ioutil"
 	"log"
+	"os"
 	"os/exec"
 	"strconv"
 	"time"
@@ -12,7 +14,8 @@ import (
 //ffmpeg -i "rtsp://admin:YHDYPD@192.168.184.180:554/h264/ch1/main/av_stream" -r 1  -y /home/eagle/rtmp/images/image%d.jpg 每秒截取一张图片
 //ffmpeg -y -i "rtmp://58.200.131.2:1935/livetv/hunantv" -ss 00:00:01 -vframes 1 -f image2 /home/eagle/rtmp/images/image1.jpg
 
-const RtspUrl = "rtsp://admin:YHDYPD@192.168.184.180:554/h264/ch1/main/av_stream"
+//const RtspUrl = "rtsp://admin:YHDYPD@192.168.184.180:554/h264/ch1/main/av_stream"
+const RtspUrl = "rtmp://58.200.131.2:1935/livetv/hunantv"
 const ImgRootUrl = "/home/eagle/rtmp/"
 
 var classifyPID int
@@ -38,6 +41,8 @@ func VideoCaptureStart1(imgName string, imgDir string, ch chan<- string) {
 	err := mkdir.Run()
 	if err != nil {
 		log.Println("mkdir erro:%v", err)
+		ch <- fmt.Sprintf("mkdir erro:%v", err)
+		return
 	}
 	img = img + "/" + imgName
 	command := fmt.Sprintf(commandFmt, RtspUrl, img)
@@ -72,10 +77,33 @@ func VideoCaptureStop1(pid string, ch chan<- string) {
 }
 
 func GetLatestImage() string {
-	imgDir := strconv.Itoa(time.Now().Hour()) + "-" + strconv.Itoa(time.Now().Day()) + "-" + time.Now().Month().String() + "-" + strconv.Itoa(time.Now().Year())
-	files, err := ioutil.ReadDir(ImgRootUrl + imgDir + "/")
-	if err != nil {
-		log.Println("read Dir error")
+	timeNow := time.Now()
+	imgDirNow := strconv.Itoa(timeNow.Hour()) + "-" + strconv.Itoa(timeNow.Day()) + "-" + timeNow.Month().String() + "-" + strconv.Itoa(timeNow.Year())
+	files, _ := ioutil.ReadDir(ImgRootUrl + imgDirNow + "/")
+	rec := "error"
+	if len(files) > 0 {
+		//当前小时才刚刚开始，没有文件写入
+		fmt.Println("maxNum:", len(files))
+		rec = ImgRootUrl + imgDirNow + "/" + "classify" + strconv.Itoa(len(files)) + ".jpg"
+	} else {
+		timeLast := timeNow.Add(-time.Hour)
+		//查找上一个小时的最新文件返回，这里存在一个BUG,晚上00：00时会触发
+		imgDirLast := strconv.Itoa(timeLast.Hour()) + "-" + strconv.Itoa(timeLast.Day()) + "-" + timeLast.Month().String() + "-" + strconv.Itoa(timeLast.Year())
+		files, _ = ioutil.ReadDir(ImgRootUrl + imgDirLast + "/")
+		if len(files) > 0 {
+			fmt.Println("maxNum:", len(files))
+			rec = ImgRootUrl + imgDirLast + "/" + "classify" + strconv.Itoa(len(files)) + ".jpg"
+		}
 	}
-	return imgDir + "/" + files[len(files)].Name()
+	timeOld := timeNow.Add(-time.Hour * 2)
+	imgDirOld := strconv.Itoa(timeOld.Hour()) + "-" + strconv.Itoa(timeOld.Day()) + "-" + timeOld.Month().String() + "-" + strconv.Itoa(timeOld.Year())
+	log.Println(ImgRootUrl + imgDirOld)
+	os.Remove(ImgRootUrl + imgDirOld)
+	return rec
+}
+
+func GetLatestImage1(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"message": GetLatestImage(),
+	})
 }
