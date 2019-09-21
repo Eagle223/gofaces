8a0489862d779ff448a1029075105e6e23a0645e
Eagle223 <m18875144703@163.com>
2019-09-21 13:15:48 +0800

    完善了README.md文件

--- rtsp/rtsp.go
+++ rtsp/rtsp.go
@@ -10,11 +10,27 @@ import (
 	"time"
 )
 
+//ffmpeg -i "rtsp://admin:YHDYPD@192.168.184.180:554/h264/ch1/main/av_stream" -r 1  -y /home/eagle/rtmp/images/image%d.jpg 每秒截取一张图片
+//ffmpeg -y -i "rtmp://58.200.131.2:1935/livetv/hunantv" -ss 00:00:01 -vframes 1 -f image2 /home/eagle/rtmp/images/image1.jpg
+//ffmpeg -y -i "rtmp://58.200.131.2:1935/livetv/hunantv" -r 1  -y /home/gofaces/rtmp/images/image%d.jpg
+//const RtspUrl = "rtsp://admin:YHDYPD@192.168.184.180:554/h264/ch1/main/av_stream"
 const RtspUrl = "rtsp://admin:YHDYPD@192.168.184.180:554/h264/ch1/main/av_stream"
 const ImgRootUrl = "/home/gofaces/rtmp/"
 
 var classifyPID int
 
+/*
+* ffmpeg 参数设置
+* -i 输入流或者文件
+* -r 设置帧数Hz为单位
+* -s 设置帧大小（WxH）
+* 设计思路：
+ */
+
+/*
+* 从视频中截取图片
+*
+ */
 func VideoCaptureStart1(imgName string, imgDir string, ch chan<- string) {
 	commandFmt := "ffmpeg  -y -i \"%v\" -r 1 %v"
 	img := ImgRootUrl + imgDir
@@ -42,6 +58,9 @@ func VideoCaptureStart1(imgName string, imgDir string, ch chan<- string) {
 	mkdir.Wait()
 }
 
+/*
+* 安全的结束ffmpeg进程
+ */
 func VideoCaptureStop1(pid string, ch chan<- string) {
 	cmd := exec.Command("sh", "-c", "kill "+pid)
 	err := cmd.Run()
